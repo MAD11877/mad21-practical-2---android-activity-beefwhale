f5e5445ef9ccd9247c8645671b1a741e8e6882bb
Yongqing Wang <s10222015@connect.np.edu.sg>
2022-05-24 09:25:26 +0800

    Init user object #2 fixed

--- app/src/main/java/com/example/madpractical/MainActivity.java
+++ app/src/main/java/com/example/madpractical/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.madpractical;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -14,6 +19,14 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView2 = findViewById(R.id.textView2);
+        textView2.setText("Hello World!");
+
+        TextView textView3 = findViewById(R.id.textView3);
+        textView3.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+
     }
 
     public void onClick(View view) {
